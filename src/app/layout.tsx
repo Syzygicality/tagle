4b3a02c041b3e5571176cfc8c50a924dfc596b62
Edison Wang <ew040607@gmail.com>
2026-04-21 00:24:16 -0400

    fix: bootstrap setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
+import BootstrapClient from "@/components/BootstrapClient";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +27,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="flex min-h-full flex-col">
+        <BootstrapClient />
+        {children}
+      </body>
     </html>
   );
 }
